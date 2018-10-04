05adb23c6f58026d6a5571d54c6b6b63d1313240
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-10-04 18:02:46 -0400

    fixed few bugs in BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,48 +1,48 @@
 public class BankAccount{
-double balance;
-int accountID;
-String password;
-public void Values(double NewB, int NewID, String NewPassword){
-balance = NewB;
-accountID = NewID;
-password = NewPassword;
-}
-public double getID()
-{
-return BankAccount.accountID;
-}
-public String getcode()
-{
-return BankAccount.password;
-}
-public double getBalance()
-{
-return BankAccount.balance;
-}
-public void setCode(String newCode)
-{
-password = newCode;
-}
-public String toString()
-{
-  return "" + BankAccount.balance + "                   " +  BankAccount.accountID;
-}
-public boolean Deposit(int dep)
-{
-if (BankAcccount.balance > 0)
-{
-BankAccount.balance += dep;
-return true;
-}
-return false;
-}
-public boolean Withdraw(int out)
-{
-if (BankAccount.balance >= out)
-{
-BankAccount.balance -= out;
-return true;
-}
-return false;
-}
+    double balance;
+    int accountID;
+    String password;
+    public BankAccount( double NewB, int NewID, String NewPassword){
+        balance = NewB;
+        accountID = NewID;
+        password = NewPassword;
+    }
+    public double getID()
+    {
+        return accountID;
+    }
+    public String getcode()
+    {
+        return password;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void setCode(String newCode)
+    {
+        password = newCode;
+    }
+    public String toString()
+    {
+        return "" + balance + "                   " +  accountID;
+    }
+    public boolean deposit(double dep)
+    {
+        if (balance > 0)
+        {
+            balance += dep;
+            return true;
+        }
+        return false;
+    }
+    public boolean withdraw(double out)
+    {
+        if (balance >= out)
+        {
+            balance -= out;
+            return true;
+        }
+        return false;
+    }
 }
