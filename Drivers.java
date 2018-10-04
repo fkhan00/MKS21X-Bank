37478e30a21451185b11c47f473d73e983826bb2
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-10-04 18:03:44 -0400

    executed Drivers.java it worked

--- Drivers.java
+++ Drivers.java
@@ -1,12 +1,38 @@
 public class Drivers{
-    public static void main(){
-      BankAccount a = new BankAccount(balance: 0, accountID: 0, password: "" );
-      a.Values(4815162342.0, 498340234, "drowssap");
-      System.out.println(a.getID(), a.getBalance(), a.getCode());
-      System.out.println(a.toString());
-      Deposit(15);
-      System.out.println(a.getBalance());
-      Withdraw(23);
-      System.out.println(a.getBalance());
+    public static void main(String[]args){
+        BankAccount b1 = new BankAccount(1000.0, 1000001, "abc123");
+        BankAccount b2 = new BankAccount(1234.0, 1000231, "abcasdg23");
+        
+        System.out.println("Account b1:");
+        System.out.println(b1);
+        System.out.println("Account b2:");
+        System.out.println(b2);
+        System.out.println();
+        
+        
+        double cashAmount = 300.0;
+        while(cashAmount < 3000){
+            System.out.println("Attempt to move "+cashAmount+" from the b1 to b2 account:");
+            if (b1.withdraw(cashAmount) ){
+                if (b2.deposit(cashAmount)){
+                    System.out.println("Success");
+                }else{
+                    //This should never happen.
+                    //Error message provided to make sure that is the case
+                    System.out.println(cashAmount + " withdrawn from" +
+                                       b1 + "Failed to add to "+b2);
+                }
+            }else{
+                System.out.println("Failed to withdraw "+cashAmount+" from "+b1);
+            }
+            
+            System.out.println("Account b1:");
+            System.out.println(b1);
+            System.out.println("Account b2:");
+            System.out.println(b2);
+            System.out.println();
+            cashAmount *= 2;
+        }
     }
 }
+
