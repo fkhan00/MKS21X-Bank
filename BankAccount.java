52e6e41345fa668277c1fa1633c6a1e57828f09b
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-10-04 19:01:44 -0400

    Added Transfer method

--- BankAccount.java
+++ BankAccount.java
@@ -45,4 +45,29 @@ public class BankAccount{
         }
         return false;
     }
+    private boolean authenticate(String password){
+        return this.password == password;
+    }
+    public boolean transferTo(BankAccount other, double amount, String password)
+    {
+        if (this.authenticate(password) && this.withdraw(amount))
+        {
+            System.out.println("Attempt to move " + amount + " from the b1 to b2 account:");
+            if (other.deposit(amount))
+            {
+                System.out.println("Success");
+                return true;
+            }
+            else
+            {
+                System.out.println(amount + " withdraw from " + accountID + " Failed to add to " +  other);
+                return false;
+            }
+        }
+        else
+        {
+            System.out.println("Failed to withdraw " + amount + " from " + accountID);
+            return false;
+        }
+    }
 }
