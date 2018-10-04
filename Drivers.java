9e6f9b845906de6c235abe31139517069ded3d0e
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-10-04 19:02:22 -0400

    Successfully tested transfer method

--- Drivers.java
+++ Drivers.java
@@ -11,28 +11,21 @@ public class Drivers{
         
         
         double cashAmount = 300.0;
-        while(cashAmount < 3000){
-            System.out.println("Attempt to move "+cashAmount+" from the b1 to b2 account:");
-            if (b1.withdraw(cashAmount) ){
-                if (b2.deposit(cashAmount)){
-                    System.out.println("Success");
-                }else{
-                    //This should never happen.
-                    //Error message provided to make sure that is the case
-                    System.out.println(cashAmount + " withdrawn from" +
-                                       b1 + "Failed to add to "+b2);
-                }
-            }else{
-                System.out.println("Failed to withdraw "+cashAmount+" from "+b1);
-            }
-            
-            System.out.println("Account b1:");
-            System.out.println(b1);
-            System.out.println("Account b2:");
-            System.out.println(b2);
-            System.out.println();
-            cashAmount *= 2;
-        }
+        b1.transferTo(b2, 318, "abc123");
+        System.out.println("Account b1:");
+        System.out.println(b1);
+        System.out.println("Account b2:");
+        System.out.println(b2);
+        System.out.println();
+        
+        b1.transferTo(b2, 318, "password");
+        System.out.println("Account b1:");
+        System.out.println(b1);
+        System.out.println("Account b2:");
+        System.out.println(b2);
+        System.out.println();
+ja
     }
 }
+
 
